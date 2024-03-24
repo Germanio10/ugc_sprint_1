964cc4f598e1feffb2965abd0fae32adc7d15efa
German Chaley <chaleyg2@gmail.com>
2024-03-24 19:58:34 +0300

    Метрики Clickhouse и Vertica

---
+++ metrics/clickhouse/generate_fake_data.py
@@ -0,0 +1,32 @@
+import clickhouse_driver
+
+
+def generate_fake_data(conn, table_name, count_data):
+    with conn.cursor() as cursor:
+        insert_data = f"INSERT INTO {table_name} (id, user_id, view_time, event_time) VALUES"
+
+        for i in range(1, count_data + 1):
+            id = "rand()"
+            user_id = "rand()"
+            view_time = "rand()"
+            event_time = "today()"
+            insert_data += (f"({id}, {user_id}, {view_time}, {event_time}),")
+
+            if i % 10000 == 0:
+                insert_data = insert_data[:-1]
+                cursor.execute(insert_data)
+                insert_data = f"INSERT INTO {table_name} (id, user_id, view_time, event_time) VALUES"
+
+        conn.commit()
+
+
+if __name__ == "__main__":
+    conn = clickhouse_driver.connect(host='localhost', database='default')
+    cursor = conn.cursor()
+    generate_fake_data(conn, 'test', 10000000)
+    cursor.execute("SELECT COUNT(*) FROM test")
+    count = cursor.fetchone()[0]
+    print(f'Total rows in the table: {count}')
+
+    conn.close()
+
---
+++ metrics/vertica/README.md
@@ -0,0 +1,39 @@
+## Без нагрузки
+
+Function: insert_100_rows
+Execution time: 0.06171 seconds
+
+Function: insert_1000_rows
+Execution time: 0.03510 seconds
+
+Function: insert_10000_rows
+Execution time: 0.15457 seconds
+
+Function: get_users
+Execution time: 47.82784 seconds
+
+Function: get_sum_view_time
+Execution time: 0.21860 seconds
+
+Function: get_sum_view_time_group_users
+Execution time: 57.14469 seconds
+
+## С Нагрузкой
+
+Function: insert_100_rows
+Execution time: 0.06360 seconds
+
+Function: insert_1000_rows
+Execution time: 0.02629 seconds
+
+Function: insert_10000_rows
+Execution time: 0.15758 seconds
+
+Function: get_users
+Execution time: 49.51061 seconds
+
+Function: get_sum_view_time
+Execution time: 0.09599 seconds
+
+Function: get_sum_view_time_group_users
+Execution time: 79.87324 seconds
