8ea3401bec812d9deb641af9915a245b43fb31b2
SekretovKirill <sekir2011@gmail.com>
2024-03-28 20:03:43 +0300

    Ручка преобразована к новой структуре

---
+++ ugc_api/src/services/filter_service.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from functools import lru_cache
+from fastapi import Depends
+from producers.abstract_producer import AbstractProducer
+from models.filter import FilterEventDTO, FilterProduceEventDTO
+from producers.kafka_producer import get_producer
+from services.base_service import BaseService
+from models.user import User
+
+
+class SearchFilterSevice(BaseService):
+    def __init__(self, producer: AbstractProducer) -> None:
+        self.producer = producer
+        self.topic = 'messages'
+
+    async def execute(self, search_filter: FilterEventDTO, user: User) -> FilterProduceEventDTO:
+
+        search_filter = FilterProduceEventDTO(
+            user_id=user.user_id,
+            produce_timestamp=datetime.now(),
+            **search_filter.model_dump(),
+        )
+
+        key = self._get_key('filter', search_filter, include_fields=['user_id', 'genre_id', 'produce_timestamp',])
+        message = self._get_message(search_filter)
+        await self.producer.send(self.topic, key, message)
+
+        return search_filter
+
+
+@lru_cache
+def get_search_filter_servece(
+    producer: AbstractProducer = Depends(get_producer)
+) -> SearchFilterSevice:
+    return SearchFilterSevice(producer=producer)
